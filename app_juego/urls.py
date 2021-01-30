3e36e67dc7bfe56eaf8b1d8dc561734c6ce50f1a
TaladropoliS <fernando.taladriz@gmail.com>
2021-01-30 11:36:42 -0300

    actualizacion a un solo metodo con session

--- app_juego/urls.py
+++ app_juego/urls.py
@@ -3,8 +3,4 @@ from . import views
 urlpatterns = [
     path('', views.inicio),
     path('process_money', views.process_money),
-    path('granja', views.granja),
-    path('cueva', views.cueva),
-    path('casa', views.casa),
-    path('casino', views.casino),
 ]
